dba6250674ed806e5cbac33b761686d67d9dfbb2
klein.zhou <klein.zhou@quectel.com>
2019-01-09 13:46:36 +0800

    Add font Consolas and si_activate.txt
    Signed-off-by: klein.zhou <klein.zhou@quectel.com>

---
+++ config/font/install_font.sh
@@ -0,0 +1,5 @@
+sudo mkdir -p /usr/share/fonts/consolas
+sudo cp YaHei.Consolas.1.12.ttf /usr/share/fonts/consolas/
+sudo chmod 644 /usr/share/fonts/consolas/YaHei.Consolas.1.12.ttf
+cd /usr/share/fonts/consolas
+sudo mkfontscale && sudo mkfontdir && sudo fc-cache -fv &&cd -
